b56543d75d4ea8182ed437e50dad6f023226e9bf
Benjamin Riot Bretecher <binmenja@beluga2.int.ets1.calculquebec.ca>
2024-09-01 17:50:46 -0400

    update 8mn avg

--- nsa_dataset_processing_8mn_average.m
+++ nsa_dataset_processing_8mn_average.m
@@ -10,7 +10,7 @@ load(filewnum)
 load("noise_review.mat") % threshold here is 2.4489 RU instead of 1 RU, keeping around 70% of the data
 %load("noise_2_review.mat")%more conservative noise correction, 2 RU threshold
 addpath('/lustre03/project/6003571/binmenja/matlab/mylib/')
-for iyear=1:26
+for iyear=24:26
 disp(iyear)
     for imonth=1:12
         disp(imonth)
@@ -25,7 +25,7 @@ disp(iyear)
         clearvars -except pathwork iyear turner_985 percent_discarded imonth month year wnum_resp month_count threshold_ts threshold_bt_std spectra_count day day_str hour_str resp responsivity_monthly rad rad_std radiance_monthly nsaC1_wnum hourly_time_monthly monthly_time nsaC1_lwskynen_fixed dateStringsArray noise_corrected noise_corrected_2;
 
         %filefolder=strcat('/Users/benjaminriot/Desktop/nsaC1_total_','2014','02');
-        for nenCase = 0:1
+        for nenCase = 1
             filefolder = strcat('/home/binmenja/direct/aeri/nsa/2023_rolls_2/data_total/nsaC1_total_', num2str(year(iyear)), num2str(month(imonth)));
             filename = strcat(filefolder, '/nsaC1_total.mat');
             load(filename)
@@ -43,6 +43,7 @@ disp(iyear)
                 disp(length(nsaC1_total.hatch))
                 disp('Sum of discarding based on hatch or noise - classic version:')
                 disp(sum(discard))
+
             end
         
 
@@ -52,7 +53,11 @@ disp(iyear)
             condition_mask = abs(nsaC1_total.lw_nesr_extrapolated) > abs(nsaC1_total.radiance);
         
             % Combine the two conditions: radiance <= 0 and condition_mask
-            combined_mask = (nsaC1_total.radiance <= 0) & condition_mask;
+            combined_mask = (nsaC1_total.radiance <= 0 & nsaC1_total.lw_nesr_extrapolated <0) & condition_mask;
+
+            condition_mask2 = any(nsaC1_total.lw_nesr_extrapolated <0,1);
+            disp('Sum of discarding based on nesr <0:')
+            disp(sum(condition_mask2))
             %disp(combined_mask)
 
 
@@ -63,9 +68,52 @@ disp(iyear)
             nsaC1_total.time(discard) = NaN;
             nsaC1_total.airTemp(discard) = NaN;
             nsaC1_total.hatch(discard) = NaN;
+            nsaC1_total.lwskynen(discard) = NaN;
+
+
+            nsaC1_total.radiance(:, condition_mask2) = NaN;
+            nsaC1_total.lw_nesr_extrapolated(:, condition_mask2) = NaN;
+            nsaC1_total.time(condition_mask2) = NaN;
+            nsaC1_total.airTemp(condition_mask2) = NaN;
+            nsaC1_total.hatch(condition_mask2) = NaN;
+            nsaC1_total.lwskynen(condition_mask2) = NaN;
+
+
 
             nsaC1_total.radiance(combined_mask) = NaN;
             nsaC1_total.lw_nesr_extrapolated(combined_mask) = NaN;
+
+            cond = false; % for when i want to add the small radiance condition
+            countt = 0;
+            if cond
+                [~, wn_982] = min(abs(nsaC1_wnum-982));
+                [~, wn_987] = min(abs(nsaC1_wnum-987));
+                
+                num_spectra = size(nsaC1_total.radiance, 2);
+                
+                for i = 1:num_spectra
+                    noise_value = mean(nsaC1_total.lw_nesr_extrapolated(wn_982:wn_987, i), 1, 'omitnan');
+                    
+                    if ~isnan(noise_value)
+                        if mean(nsaC1_total.radiance(wn_982:wn_987, i), 1) < abs(2 * noise_value)
+                            countt = countt + 1;
+                            % Mark the low radiance spectra
+                            nsaC1_total.radiance(:, i) = NaN;
+                            nsaC1_total.lw_nesr_extrapolated(:, i) = NaN;
+                            nsaC1_total.time(i) = NaN;
+                            nsaC1_total.airTemp(i) = NaN;
+                            nsaC1_total.hatch(i) = NaN;
+                            nsaC1_total.lwskynen(i) = NaN;
+                        end
+                    %else
+                        %disp(['No low radiance condition applied for spectrum ', num2str(i)]);
+                    end
+                end
+                
+                disp('Sum of discarding based on low radiance:')
+                disp(countt)  % Count the number of discarded spectra
+            end
+
 
             
             
@@ -77,6 +125,7 @@ disp(iyear)
             nsaC1_8mn.rad = NaN(2904,counting_total);
             nsaC1_8mn.rad_std = NaN(2904,counting_total);
             nsaC1_8mn.lw_nesr_extrapolated = NaN(2904,counting_total);
+            
                 %nsaC1_average.resp = NaN(length(wnum_resp),counting_total);
             nsaC1_8mn.second = NaN;
             nsaC1_8mn.airTemp = NaN;
@@ -90,7 +139,7 @@ disp(iyear)
                 index = find((nsaC1_total.second<initial_second+h*480)&(nsaC1_total.second>=initial_second+(h-1)*480) & ~isnan(nsaC1_total.time));
                 nsaC1_8mn.second(h) = initial_second+240+(h-1)*480;
                 nsaC1_8mn.date(h) = datetime(1970,1,1,0,0,0,'Format','yyyyMMddHHmm')+seconds(nsaC1_8mn.second(h));
-                if (~isempty(index)) & (length(index) >= 3)
+                if (~isempty(index)) %& (length(index) >= 3)
                         rad_select = nsaC1_total.radiance(:,index);
                         
                         % Add a line to delete spectra where NaN values exist
@@ -122,7 +171,14 @@ end
                 save(fullfile(pathwork,'nsaC1_8mn_adj.mat'),'nsaC1_8mn', '-v7.3');
                 disp('adjusted version saved')
             else 
-                save(fullfile(pathwork,'nsaC1_8mn.mat'),'nsaC1_8mn', '-v7.3');  
+                if cond
+                    save(fullfile(pathwork,'nsaC1_8mn_lowrad.mat'),'nsaC1_8mn', '-v7.3');
+                    disp('classic version saved')
+                else
+                    save(fullfile(pathwork,'nsaC1_8mn.mat'),'nsaC1_8mn', '-v7.3');
+                    disp('classic version saved')
+                end
+                
                 disp('original version saved')      
             end
         end
